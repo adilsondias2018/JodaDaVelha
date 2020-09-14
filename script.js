9489d765ca6ec76f193a105bb222fe0927a93ca4
Adilson Dias <adilsondias@outlook.com>
2020-09-14 20:05:31 -0300

    Tabuleiro montado

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ const winningCombinations = [
     [1, 5, 9],
     [3, 5, 7]
 ];
+
+const handleClick = function(event) {
+    const cell = event.target
+    console.log(cell.id);
+}
+const cells = document.querySelectorAll('td');
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', handleClick);
+}
